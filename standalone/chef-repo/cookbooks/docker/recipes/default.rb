8d94041b70de6872592134e294b03346fa3cd585
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2023-10-29 13:26:28 +0900

    Ensure running docker-registry

--- standalone/chef-repo/cookbooks/docker/recipes/default.rb
+++ standalone/chef-repo/cookbooks/docker/recipes/default.rb
@@ -46,17 +46,17 @@ service 'docker' do
   action [:enable, :start]
 end
 
-#template '/etc/default/docker' do
-#  source 'etc/default/docker.erb'
-#  owner 'root'
-#  group 'root'
-#  mode 0644
-#  notifies :restart, 'service[docker]'
-#end
-#
-#bash 'ensure docker registry (v2) is running' do
-#  code <<-EOH
-#    docker run -d -p #{node['docker']['registry_port']}:5000 --name registry registry:2
-#  EOH
-#  only_if "test $(docker ps -aq -f name=registry | wc -l) -eq 0"
-#end
+template '/etc/default/docker' do
+  source 'etc/default/docker.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, 'service[docker]'
+end
+
+bash 'ensure docker registry (v2) is running' do
+  code <<-EOH
+    docker run -d -p #{node['docker']['registry_port']}:5000 --name registry registry:2
+  EOH
+  only_if "test $(docker ps -aq -f name=registry | wc -l) -eq 0"
+end
